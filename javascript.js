3e9bf9aa5d2d14d1deb8f07a751c59f40410aacc
ElwDimension <ElwDimension@gmail.com>
2023-03-21 14:44:05 -0400

    Rearraged some variables

--- javascript.js
+++ javascript.js
@@ -1,14 +1,11 @@
-//let answer=prompt("Please enter either Rock, Paper, or Scissors.");
-//let playerSelection=answer.toLowerCase().trim();
-let computerSelection=getComputerChoice();
-
-
+let playerScore=0;
+let computerScore=0;
+game();
 
 
 
 function getComputerChoice(){
-    let x=Math.random();
-    console.log(x);
+    let x=Math.floor(Math.random() * 3);
     if(x==0){
         return "rock"
     }
@@ -22,10 +19,35 @@ function getComputerChoice(){
 
 function round(playerSelection, computerSelection){
     if(playerSelection==="rock"&&computerSelection==="scissors" || playerSelection==="paper"&&computerSelection==="rock" || playerSelection==="scissors"&&computerSelection==="paper"){
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
+        playerScore+=1;
+        return `You win! ${playerSelection} beats ${computerSelection}!`;  
     }
     else{
+        computerScore+=1;
         return `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
+}
+
+function game(){
+    playerScore=0;
+    computerScore=0;
+
+    for (let i=0;i<5;i++){
+        let answer=prompt("Please enter either Rock, Paper, or Scissors.");
+        let playerSelection=answer.toLowerCase().trim();
+        let computerSelection=getComputerChoice();
+        console.log(round(playerSelection,computerSelection));
+    }
+
+    console.log(`Player Score: ${playerScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+
+    if(playerScore>computerScore){
+        console.log(`Congrats! You win!`)
+    }
+    else{
+        console.log(`Sorry! Computer won!`)
+    }
+
 }
 
